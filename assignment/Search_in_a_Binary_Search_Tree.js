3585996a85842edaebf65524743105e896398a38
Md.Ashikur Rahman (Showrov) <showrov.cse.cou@gmail.com>
2023-09-25 16:20:09 +0600

    Search in a Binary Search Tree and reurned subtree

---
+++ assignment/Search_in_a_Binary_Search_Tree.js
@@ -0,0 +1,19 @@
+/**
+ ** Time Complexity O()
+ ** Space Complexity O(1)
+ */
+
+const searchBST = (root, val) => {
+  if (!root) {
+    return null;
+  }
+  if (root.val === val) {
+    return root;
+  }
+  if (val > root.val) {
+    return searchBST(root.right, val);
+  }
+  if (val < root.val) {
+    return searchBST(root.left, val);
+  }
+};
