f461329d99a6ffbd5a9424b56bd5e737e5489abf
Md.Ashikur Rahman (Showrov) <showrov.cse.cou@gmail.com>
2023-11-06 20:27:57 +0600

    Best Time to Buy and Sell Stock

---
+++ assignment/Best_Time_to_Buy_and_Sell_Stock.js
@@ -0,0 +1,16 @@
+const maxProfit = (prices) => {
+  let maxProfit = 0;
+  let minPrice = prices[0];
+
+  for (let sell = 1; sell < prices.length; sell++) {
+    let sellPrice = prices[sell];
+    let profit = sellPrice - minPrice;
+
+    maxProfit = Math.max(maxProfit, profit);
+
+    if (sellPrice < minPrice) minPrice = sellPrice;
+  }
+  return maxProfit;
+};
+
+console.log(maxProfit([1, 2, 3, 4, 5]));
